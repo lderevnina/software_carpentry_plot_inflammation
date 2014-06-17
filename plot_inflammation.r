dc4aad336ffe2c40cad5799fcd0b21d47e1d79a2
Lida Derevnina <lida.derevnina8@gmail.com>
2014-06-17 13:27:32 -0700

    added unverscore to the comment

--- plot_inflammation.r
+++ plot_inflammation.r
@@ -18,7 +18,7 @@ data.in <- read.csv(options[1])
 # melt with defaults
 data.in.melted <- melt(data.in)
 
-#create a plot with GGPLOT 
+#create a plot with GGPLOT _ 
 out.plot <- ggplot(data.in.melted,(aes(x=PatientID, y=value))) + 
     geom_point() + 
     stat_sum() +
